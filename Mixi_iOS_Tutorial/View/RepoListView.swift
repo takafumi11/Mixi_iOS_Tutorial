9a2af55b9e45fba7b5563a845c4b4793648a2a9e
takafumi11 <takafumi_noiri@dirigio.jp>
2022-05-01 19:39:47 +0900

    ロード中にProgressView表示。画面の表示を1秒後に変更

--- Mixi_iOS_Tutorial/View/RepoListView.swift
+++ Mixi_iOS_Tutorial/View/RepoListView.swift
@@ -8,22 +8,37 @@
 import SwiftUI
 
 struct RepoListView: View {
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
+    @State var mockRepos: [Repo] = []
+    let isProgressHidden: Bool = false
     
     var body: some View {
         NavigationView {
-            List(mockRepos) { repo in
-                NavigationLink(
-                    destination: RepoDetailView(repo:repo)) {
-                        RepoRowView(repo: repo)
-                    }                
+            if mockRepos.isEmpty {
+                ProgressView("loading")
+            } else {
+                List(mockRepos) { repo in
+                    NavigationLink(
+                        destination: RepoDetailView(repo:repo)) {
+                            RepoRowView(repo: repo)
+                        }
+                }
+                .navigationTitle("Repositories")
             }
-            .navigationTitle("Repositories")
+            
+        }.onAppear {
+            loadRepos()
         }
-        
     }
+    
+    private func loadRepos() {
+        // 1秒後にモックデータを読み込む
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            mockRepos = [
+                .mock1, .mock2, .mock3, .mock4, .mock5
+            ]
+        }
+    }
+    
 }
 
 struct RepoListView_Previews: PreviewProvider {
